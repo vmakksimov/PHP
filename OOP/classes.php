43cd2cd2003cf4f90fe90df452047cd0de2c7920
Viktor <vmakksimov@gmail.com>
2023-11-04 17:35:04 +0200

    PHP OOP

--- OOP/classes.php
+++ OOP/classes.php
@@ -6,7 +6,7 @@ require_once ("Account.php");
 $myAccount = new Account('Viktor', 0.55);
 $otherAccount = new Account('Aneta', 50.55);
 
-
+$myAccount?->deposit(150.20)->deposit(250.20);
 
 var_dump($myAccount -> balance);
 var_dump($otherAccount -> balance);
